c717362237dc71f7f411ef859756bb9dc8fdbfb4
claire <claire@compusec.co.uk>
2025-09-01 21:18:57 +0100

    feat: add intent dispatcher to set us up to be able to dispatch to different things based on the thing we receive

--- lambda-backend/cmd/main.go
+++ lambda-backend/cmd/main.go
@@ -7,9 +7,23 @@ import (
 
 //TODO: stripped back to basics to just get that to work first.
 
-// Handler is the lambda hander
-func Handler() (alexa.Response, error) {
-	return alexa.NewSimpleResponse("testing", "Hello from Lambda!"), nil
+func IntentDispatcher(request alexa.Request) alexa.Response {
+	var response alexa.Response
+	switch request.Body.Intent.Name {
+	default:
+		response = HandleGeneric()
+	}
+
+	return response
+}
+
+func HandleGeneric() alexa.Response {
+	return alexa.NewSimpleResponse("testing", "Hello from Lambda!")
+}
+
+// This is the specific lambda handler for a request coming in
+func Handler(request alexa.Request) (alexa.Response, error) {
+	return IntentDispatcher(request), nil
 }
 
 func main() {
