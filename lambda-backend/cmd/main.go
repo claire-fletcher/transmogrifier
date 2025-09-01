2a59c3915ebafc981bbe122b5b81207e57646d84
claire <claire@compusec.co.uk>
2025-09-01 22:04:28 +0100

    feat: add some basic logging for debug which would be removed later in development

--- lambda-backend/cmd/main.go
+++ lambda-backend/cmd/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"log"
+
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/claire-fletcher/transmogrifier/internal/alexa"
 )
@@ -8,6 +10,9 @@ import (
 //TODO: stripped back to basics to just get that to work first.
 
 func IntentDispatcher(request alexa.Request) alexa.Response {
+
+	log.Printf("Received request: %+v", request) // TODO: Debugging
+
 	var response alexa.Response
 	switch request.Body.Intent.Name {
 	default:
@@ -23,6 +28,8 @@ func HandleGeneric() alexa.Response {
 
 // This is the specific lambda handler for a request coming in
 func Handler(request alexa.Request) (alexa.Response, error) {
+	log.Printf("Handler received request: %+v", request) // TODO: Debugging
+
 	return IntentDispatcher(request), nil
 }
 
