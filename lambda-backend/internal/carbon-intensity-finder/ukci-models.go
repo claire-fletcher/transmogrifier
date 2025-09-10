5cde7f3e759f7492d05d45b57f0c091ee240c923
claire <claire@compusec.co.uk>
2025-09-10 21:55:47 +0100

    feat: adjust the struct to use separate struct types instead of nesting, for ease of creating other structs

--- lambda-backend/internal/carbon-intensity-finder/ukci-models.go
+++ lambda-backend/internal/carbon-intensity-finder/ukci-models.go
@@ -1,13 +1,17 @@
 package CarbonIntensityFinder
 
 type UKCIResponse struct {
-	Data []struct {
-		From      string `json:"from"`
-		To        string `json:"to"`
-		Intensity struct {
-			Forecast int    `json:"forecast"`
-			Actual   int    `json:"actual"`
-			Index    string `json:"index"`
-		} `json:"intensity"`
-	} `json:"data"`
+	Data Data `json:"data"`
+}
+
+type Data struct {
+	From      string    `json:"from"`
+	To        string    `json:"to"`
+	Intensity Intensity `json:"intensity"`
+}
+
+type Intensity struct {
+	Forecast int    `json:"forecast"`
+	Actual   int    `json:"actual"`
+	Index    string `json:"index"`
 }
